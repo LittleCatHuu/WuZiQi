3532d27b3382ef1ec0dfb79e7704596d0564a6da
LittleCatHuu <zx@123126zx@163.com>
2017-06-14 23:30:29 +0800

    导入素材及完成布局

---
+++ app/src/main/java/com/example/littlecathuu/wuziqi/WuZiQiPanel.java
@@ -0,0 +1,15 @@
+package com.example.littlecathuu.wuziqi;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by n9262 on 2017/6/14.
+ */
+
+public class WuZiQiPanel extends View {
+    public WuZiQiPanel(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
