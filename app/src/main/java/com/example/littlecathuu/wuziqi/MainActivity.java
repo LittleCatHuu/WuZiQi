c64cefd42e306a3ba15cf059ddbe1e7917e7b6d3
LittleCatHuu <zx@123126zx@163.com>
2017-06-16 21:40:11 +0800

    finish

--- app/src/main/java/com/example/littlecathuu/wuziqi/MainActivity.java
+++ app/src/main/java/com/example/littlecathuu/wuziqi/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.littlecathuu.wuziqi;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
+    private WuZiQiPanel mGamePanel;
+    private AlertDialog.Builder alertBuilder;
+    private AlertDialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        alertBuilder = new AlertDialog.Builder(MainActivity.this);
+        alertBuilder.setPositiveButton("again", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                mGamePanel.restartGame();
+            }
+        });
+        alertBuilder.setNegativeButton("quit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                MainActivity.this.finish();
+            }
+        });
+        alertBuilder.setCancelable(false);
+        alertBuilder.setTitle("finish");
+        mGamePanel = (WuZiQiPanel) findViewById(R.id.id_wuziqi);
+        mGamePanel.setOnGameStatusChangeListener(new OnGameStatusChangeListener() {
+            @Override
+            public void onGameOver(int gameWinResult) {
+                switch (gameWinResult){
+                    case WuZiQiPanel.WHITE_WIN:
+                        alertBuilder.setMessage("white win");
+                        break;
+                    case WuZiQiPanel.BLACK_WIN:
+                        alertBuilder.setMessage("black win");
+                        break;
+                    case WuZiQiPanel.NO_WIN:
+                        alertBuilder.setMessage("nobody win");
+                        break;
+                }
+                alertDialog = alertBuilder.create();
+                alertDialog.show();
+            }
+        });
     }
 }
